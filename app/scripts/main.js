2e7c5a9c6658cd433808b4c85f0639101279801d
Gil Barbara <gilbarbara@gmail.com>
2015-07-12 14:46:28 -0300

    initial React views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,27 @@
 var React = require('react'),
     App  = require('./App');
 
+var showName = (e) => {
+    var el = e.toElement;
+    el.parentNode.classList.add('visible');
+};
+
+var hideName = (e) => {
+    var el = e.fromElement;
+    el.parentNode.classList.remove('visible');
+};
+
+
+var enableTips = () => {
+    var elems = document.querySelectorAll('.logo-item img');
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].addEventListener("mouseenter", showName, false);
+        elems[i].addEventListener("mouseleave", hideName, false);
+    }
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     React.render(<App/>, document.getElementById('react'));
+    enableTips();
 });
+
