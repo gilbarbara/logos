3a3ac85d7c3cf3332b2682f2fea7329128d0dd6f
Gil Barbara <gilbarbara@gmail.com>
2015-07-12 16:06:04 -0300

    initial tag cloud

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,6 @@
 var React = require('react'),
     App  = require('./App');
 
-
-
 document.addEventListener('DOMContentLoaded', function () {
     React.render(<App/>, document.getElementById('react'));
 });
-
